1da01e2ab734f85a07b5b8a7fb63a0fe4f4027f8
Sano <extravaganza1024@gmail.com>
2023-05-28 14:01:29 -0700

    React (#2)
    * install CRA resources
    
    * Cleanup; React clone

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import Area from "./components/Area";
+import Panel from "./components/Panel";
+
+function App() {
+    return (
+        <>
+            <Area />
+            <Panel />
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/Area.jsx
@@ -0,0 +1,10 @@
+
+function Area() {
+    return (
+        <label htmlFor="text_area">
+            <textarea id="text_area"></textarea>
+        </label>
+    );
+}
+
+export default Area;
